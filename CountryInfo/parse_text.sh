0d38c2c90e7d749b5524764d88ef6dae63472cc6
Merluciusz <limestoragesolutions@gmail.com>
2022-04-20 12:47:50 +0300

    check for null

--- CountryInfo/parse_text.sh
+++ CountryInfo/parse_text.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 # parseing a CSV file with field separator
+# To be used in pair with "countries.txt"
 
 # format:
 # Country Capital Population Aria Continent Neighbours
 # Moldova (Republic of)|Chișinău|3553100|33846|Europe|ROU~UKR
 
 # global variables
-OLDFSI=$IFS
+YELLOW="\e[1;33m"
+LINE_RES="\e[0m"
+OLDIFS=$IFS
 IFS="|"
+FILE=$1
 count=1
 
-while read country capital population aria continent neighbours
-do
-echo -e "\e[1;33m$count $country \
----------------------------\e[0m\n\
+function check_file() {
+    if [ ! -f ./$FILE ]; then
+        echo "The CSV file is missing"
+        exit 1
+    fi
+}
+
+check_file
+
+while read country capital population aria continent neighbours; do
+    echo -e "$YELLOW$count $country \
+---------------------------$LINE_RES\n\
 
 Capital :\t $capital\n\
 Population :\t $population\n\
@@ -21,7 +33,9 @@ Aria :\t\t $aria\n\
 Continent :\t $continent\n\
 Neigbours :\t $neighbours\n"
 
-((count++))
-done < $1
+    ((count++))
+done <$1
 
-IFS=$IFS
+IFS=$OLDIFS
+
+
